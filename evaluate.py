3e8c84e4df3ad18f9cb52c1cbaa875469089e0b1
Herbert Ullrich <herbert.ullrich@yeti-studio.cz>
2019-03-25 09:28:08 +0100

    Proccessing measurements

--- evaluate.py
+++ evaluate.py
@@ -4,7 +4,7 @@ from diacritic_restorer import HmmNgramRestorer
 
 DIR = "pretrained"
 TRAINING_FILE = "target_test.detok.txt"
-for language in ["sk","fr","es"]:
+for language in ["es", "hu", "hr", "lv", "pl", "ro"]:
     for n in [1, 2, 3, 4]:
         print("Testing {}-gram model for language {}".format(n, language))
         accuracy = HmmNgramRestorer.load("/".join(["pretrained", language, str(n) + "-gram.pickle"])) \
--- pretrain.py
+++ pretrain.py
@@ -2,8 +2,8 @@ from diacritic_restorer import HmmNgramRestorer
 
 DIR = "pretrained"
 TRAINING_FILE = "target_train.detok.txt"
-for language in ["hu","hr","ro"]:
-    for n in range(1,7):
+for language in ["lv", "pl", "ro"]:
+    for n in range(1, 7):
         print("Training {}-gram model for language {}".format(n, language))
         HmmNgramRestorer(n) \
             .train("/".join(["corpora", language, TRAINING_FILE])) \
